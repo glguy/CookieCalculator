8afcd2b432656633e0bfaa96d381870824399d7f
Eric Mertens <emertens@galois.com>
2016-12-09 19:11:19 -0800

    Add achievement updates

--- extract_upgrades.js
+++ extract_upgrades.js
@@ -1,1 +1,3 @@
 JSON.stringify(Game.UpgradesById.map(function(x) { return { name: x.name, pool: x.pool, power: x.power, price: x.basePrice, icon: x.icon } }))
+
+JSON.stringify(Game.AchievementsById.map(function(x) { return { name: x.name, pool: x.pool} }))
