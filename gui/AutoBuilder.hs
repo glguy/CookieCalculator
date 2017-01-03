2d7ae5fee7a31abc4bbeefb6af280f1ebc223c88
Eric Mertens <emertens@galois.com>
2017-01-02 18:55:31 -0800

    Add a custom exception type for AutoBuilder

--- gui/AutoBuilder.hs
+++ gui/AutoBuilder.hs
@@ -5,6 +5,14 @@ import qualified GI.Gtk as Gtk
 import qualified Data.Text as Text
 import           GHC.Generics
 import           Data.Coerce
+import           Control.Exception
+
+data AutoloaderFailure
+  = AutoloaderNoSuchObject String
+  | AutoloaderBadCast      String
+  deriving Show
+
+instance Exception AutoloaderFailure
 
 autoloadFromBuilder ::
   (GLoadBuilder (Rep a), Generic a) => Gtk.Builder -> IO a
@@ -19,14 +27,20 @@ instance GLoadBuilder f => GLoadBuilder (D1 c f) where
 instance GLoadBuilder f => GLoadBuilder (C1 c f) where
   gloadFromBuilder b = M1 <$> gloadFromBuilder b
 
+instance GLoadBuilder f => GLoadBuilder U1 where
+  gloadFromBuilder _ = return U1
+
 instance (GLoadBuilder f, GLoadBuilder g) => GLoadBuilder (f :*: g) where
   gloadFromBuilder b = (:*:) <$> gloadFromBuilder b <*> gloadFromBuilder b
 
-instance (Gtk.ManagedPtrNewtype o, Gtk.GObject o, Selector s) => GLoadBuilder (S1 s (K1 i o)) where
+instance (Gtk.ManagedPtrNewtype o, Gtk.GObject o, Selector s) =>
+  GLoadBuilder (S1 s (K1 i o)) where
+
   gloadFromBuilder builder =
      do let name   = selName (M1 Nothing :: S1 s Maybe ())
-            mb err = maybe (fail err) return
-        o <- mb ("Couldn't find " ++ name)
-             =<< Gtk.builderGetObject builder (Text.pack name)
-        p <- mb ("Couldn't cast " ++ name) =<< Gtk.castTo coerce o
+            orFail m err = maybe (throwIO err) return =<< m
+        o <- Gtk.builderGetObject builder (Text.pack name)
+                `orFail` AutoloaderNoSuchObject name
+        p <- Gtk.castTo coerce o
+                `orFail` AutoloaderBadCast name
         return (M1 (K1 p))
